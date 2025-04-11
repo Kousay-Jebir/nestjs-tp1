5ec8559cb013bf06b683b568ed75ffeed46b4f48
YasmineZerai <zeraiyasmine7@gmail.com>
2025-04-11 21:01:41 +0100

    added the admin guard to the user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AdminGuard } from 'src/auth/admin.guard';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard, AdminGuard)
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
