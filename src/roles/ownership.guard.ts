fe819c6b3e34d204897d577ed262689f9d5aa900
Kousay Jebir <144362888+Kousay-Jebir@users.noreply.github.com>
2025-04-12 09:52:06 +0100

    Create ownership.guard.ts

---
+++ src/roles/ownership.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable, ExecutionContext, CanActivate } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { Role } from "src/user/enums/role.enum";
+
+@Injectable()
+export class OwnershipOrAdminGuard implements CanActivate {
+    constructor(private reflector: Reflector) { }
+
+    canActivate(context: ExecutionContext): boolean {
+        const request = context.switchToHttp().getRequest();
+        const user = request.user;
+
+        const paramKey = this.reflector.get<string>('ownerParam', context.getHandler()) || 'userId';
+        const paramValue = request.params[paramKey];
+
+        return user.role === Role.ADMIN || user.id === paramValue;
+    }
+}
