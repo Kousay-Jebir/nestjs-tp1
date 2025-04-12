526b235c3e581eef2f5523bded9384281f6aed00
YasmineZerai <zeraiyasmine7@gmail.com>
2025-04-12 09:06:07 +0100

    added  create cv dto

--- src/cv/dto/create-cv.dto.ts
+++ src/cv/dto/create-cv.dto.ts
@@ -1,1 +1,22 @@
-export class CreateCvDto {}
+import { IsString, IsInt, IsOptional } from 'class-validator';
+
+export class CreateCvDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  firstname: string;
+
+  @IsInt()
+  age: number;
+
+  @IsString()
+  Cin: string;
+
+  @IsString()
+  Job: string;
+
+  @IsOptional()
+  @IsString()
+  path?: string;
+}
